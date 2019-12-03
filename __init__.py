4cdc2c61eeef3e0185416174c6caeff24e01d5cb
Chun Ly <astro.chun@gmail.com>
2019-12-03 09:02:59 -0700

    __init__: Define fitting_lines_dict

---
+++ __init__.py
@@ -0,0 +1,5 @@
+lambda0   = [3726.18, 4101.73, 4340.46, 4363.21, 4861.32, 4958.91, 5006.84]
+line_type = ['Oxy2', 'Balmer', 'Balmer', 'Single', 'Balmer', 'Single', 'Single']
+line_name = ['OII_3727', 'HDELTA', 'HGAMMA', 'OIII_4363', 'HBETA', 'OIII_4958', 'OIII_5007']
+
+fitting_lines_dict = {"lambda0":lambda0, "line_type":line_type, "line_name":line_name}
