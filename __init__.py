70e7a0089630e0ad411fbe7e0d1ea305666a6bb5
Chun Ly <astro.chun@gmail.com>
2019-12-03 09:15:57 -0700

    __init__: Define fitspath_reagen

--- __init__.py
+++ __init__.py
@@ -3,3 +3,5 @@ line_type = ['Oxy2', 'Balmer', 'Balmer', 'Single', 'Balmer', 'Single', 'Single']
 line_name = ['OII_3727', 'HDELTA', 'HGAMMA', 'OIII_4363', 'HBETA', 'OIII_4958', 'OIII_5007']
 
 fitting_lines_dict = {"lambda0":lambda0, "line_type":line_type, "line_name":line_name}
+
+fitspath_reagen = '/Users/reagenleimbach/Desktop/Zcalbase_gal/'
