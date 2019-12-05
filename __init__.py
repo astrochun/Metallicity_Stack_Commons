6fd3bfe1c47f3fbc88271f9bf65886721d340524
Chun Ly <astro.chun@gmail.com>
2019-12-05 16:34:26 -0700

    __init__: Define dust attenuation k values with cardelli()

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
+from chun_codes.cardelli import cardelli
+import astropy.units as u
+
 lambda0   = [3726.18, 4101.73, 4340.46, 4363.21, 4861.32, 4958.91, 5006.84]
 line_type = ['Oxy2', 'Balmer', 'Balmer', 'Single', 'Balmer', 'Single', 'Single']
 line_name = ['OII_3727', 'HDELTA', 'HGAMMA', 'OIII_4363', 'HBETA', 'OIII_4958', 'OIII_5007']
@@ -7,3 +10,7 @@ fitting_lines_dict = {"lambda0":lambda0, "line_type":line_type, "line_name":line
 fitspath_reagen = '/Users/reagenleimbach/Desktop/Zcalbase_gal/'
 
 fitspath_caroline = ''C:\\Users\\carol\\Google Drive\\'
+
+# Define k values for dust attenuation
+k_values = cardelli(lambda0 * u.Angstrom)
+k_dict   = dict(zip(line_name,k_values))
