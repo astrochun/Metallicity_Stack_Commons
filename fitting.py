f3b168a3547ce9d6d2e2f34d44bed7ae590d5b54
Chun Ly <astro.chun@gmail.com>
2019-12-23 14:17:14 -0800

    Add rms_func to fitting module #1

--- fitting.py
+++ fitting.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from . import scalefact
+
 con1 = 3728.91 / 3726.16 # Ratio of OII doublet line
 
 
@@ -65,3 +67,33 @@ def oxy2_gauss(x, xbar, s1, a1, c, s2, a2):
 
     return a1 * np.exp(-(x - xbar) ** 2 / (2 * s1 ** 2)) + c + \
            a2 * np.exp(-(x - (xbar * con1)) ** 2 / (2 * s2 ** 2))
+
+
+def rms_func(wave, dispersion, lambda_in, y0, sigma_array, mask_flag):
+    '''
+
+    Purpose:
+        Compute rms in the spectra
+
+    :param wave: numpy array containing rest wavelengths
+    :param dispersion: spectral dispersion in AA/pix
+    :param lambda_in: central wavelength of fit
+    :param y0: numpy array containing spectra in units of erg/s/cm2/AA
+    :param sigma_array: Gaussian sigma
+    :param scalefact: Spectra normalization factor
+    :param mask_flag: numpy array indicating spectra that are masked for OH
+           skyline contamintion
+
+    :return:
+    '''
+
+    x_idx = np.where((np.abs(wave - lambda_in) <= 100) & (mask_flag == 0))[0]
+
+    sigma = np.std(y0[x_idx])
+    pix = 5 * sigma_array / dispersion
+    s_pix = np.sqrt(pix)
+
+    ini_sig = s_pix * sigma * dispersion
+    RMS_pix = sigma * dispersion / scalefact
+
+    return ini_sig / scalefact, RMS_pix
