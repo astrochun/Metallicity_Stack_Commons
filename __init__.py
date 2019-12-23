5f4cdecfb354439e06bc27df7e0c7b7405d0ea97
Chun Ly <astro.chun@gmail.com>
2019-12-23 13:12:02 -0800

    Add exclude_outliers function

--- __init__.py
+++ __init__.py
@@ -14,3 +14,24 @@ fitspath_caroline = 'C:\\Users\\carol\\Google Drive\\'
 # Define k values for dust attenuation
 k_values = cardelli(lambda0 * u.Angstrom)
 k_dict   = dict(zip(line_name,k_values))
+
+
+def exclude_outliers(objno):
+    '''
+    Exclude spectra that are identified as outliers.
+
+    Generally this is because the spectra have very high S/N on the continuum.
+
+    :param objno: list or numpy array of eight-digit identifier
+
+    :return:
+     flag: numpy array of zeros and ones
+    '''
+
+    flag = np.zeros(len(objno), dtype=int)
+    bad_data = np.array(['32007727', '32101412', '42006031', '32035286', '14023705'])
+    for ii in range(len(bad_data)):
+        idx = [xx for xx in range(len(objno)) if bad_data[ii] == str(objno[xx])][0]
+        flag[idx] = 1
+
+    return flag
