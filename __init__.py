14148921b04fa0369955c15f5c20b38b53980e84
Chun Ly <astro.chun@gmail.com>
2019-12-24 12:21:39 -0800

    Update version to 0.1.0

--- __init__.py
+++ __init__.py
@@ -3,6 +3,8 @@ import astropy.units as u
 from datetime import date
 import os
 
+version = "0.1.0"
+
 lambda0   = [3726.18, 4101.73, 4340.46, 4363.21, 4861.32, 4958.91, 5006.84]
 line_type = ['Oxy2', 'Balmer', 'Balmer', 'Single', 'Balmer', 'Single', 'Single']
 line_name = ['OII_3727', 'HDELTA', 'HGAMMA', 'OIII_4363', 'HBETA', 'OIII_4958', 'OIII_5007']
