16acb11aff1038ae1d951554afc128c3c64ee9fc
Chun Ly <astro.chun@gmail.com>
2020-02-19 16:56:22 -0700

    column_names: Fix typo in bin_mzevolve_names0 (issue #25)

--- Metallicity_Stack_Commons/column_names.py
+++ Metallicity_Stack_Commons/column_names.py
@@ -4,7 +4,7 @@ bin_names0 = ['bin_ID', 'N_stack', 'Detection']
 
 indv_names0 = ['ID', 'R23', 'O32']
 
-bin_mzevolve_names0 = ['logM_min', 'logM_max', 'logM_avg', 'logLHb', 'logLHb_min', 'logLHb_max']
+bin_mzevolve_names0 = ['logM_min', 'logM_max', 'logM_avg', 'logLHb_min', 'logLHb_max', 'logLHb_avg']
 
 bin_zcalbase_names0 = ['logR23_min', 'logR23_max', 'logR23_avg', 'logO32_min', 'logO32_max', 'logO32_avg']
 
