cde2a2074da0cd9fff76e3329c1436e78e515aa9
Chun Ly <astro.chun@gmail.com>
2020-02-19 16:54:34 -0700

    column_names: Add bin_mzevolve_names0 list (issue #25)

--- Metallicity_Stack_Commons/column_names.py
+++ Metallicity_Stack_Commons/column_names.py
@@ -4,3 +4,4 @@ bin_names0 = ['bin_ID', 'N_stack', 'Detection']
 
 indv_names0 = ['ID', 'R23', 'O32']
 
+bin_mzevolve_names0 = ['logM_min', 'logM_max', 'logM_avg', 'logLHb', 'logLHb_min', 'logLHb_max']
