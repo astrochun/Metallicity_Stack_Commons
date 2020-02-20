e9af9d84ef3d6eb3804dfabe254283a87be201c5
Chun Ly <astro.chun@gmail.com>
2020-02-19 21:53:58 -0700

    column_names: Add merge_column_names function (iss #25)

--- Metallicity_Stack_Commons/column_names.py
+++ Metallicity_Stack_Commons/column_names.py
@@ -8,3 +8,24 @@ bin_mzevolve_names0 = ['logM_min', 'logM_max', 'logM_avg', 'logLHb_min', 'logLHb
 
 bin_zcalbase_names0 = ['logR23_min', 'logR23_max', 'logR23_avg', 'logO32_min', 'logO32_max', 'logO32_avg']
 
+
+def merge_column_names(*args):
+    """
+    Purpose:
+      Merges multiple lists containing column names.
+
+    Usage:
+      column_names = merge_column_names(bin_names0, indv_names0)
+
+    :param args: An undefined number of lists
+    :return merge_list:
+    """
+
+    merge_list = []
+
+    arg_count = len(args)
+    if arg_count > 0:
+        for elem in args:
+            merge_list += elem
+
+    return merge_list
