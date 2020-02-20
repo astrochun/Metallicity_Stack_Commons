e3fa946be3f7f2525294d7799f778ca97d659c70
Chun Ly <astro.chun@gmail.com>
2020-02-19 21:56:19 -0700

    column_names: Additional minor documentation (iss #25)

--- Metallicity_Stack_Commons/column_names.py
+++ Metallicity_Stack_Commons/column_names.py
@@ -1,11 +1,15 @@
 # These are common/general column names
 
+# Column names for bin information
 bin_names0 = ['bin_ID', 'N_stack', 'Detection']
 
+# Column names for individual galaxies/spectra
 indv_names0 = ['ID', 'R23', 'O32']
 
+# Column names for bin information in stellar mass and H-beta luminosity
 bin_mzevolve_names0 = ['logM_min', 'logM_max', 'logM_avg', 'logLHb_min', 'logLHb_max', 'logLHb_avg']
 
+# Column names for bin information in R23 and O32 line ratios
 bin_zcalbase_names0 = ['logR23_min', 'logR23_max', 'logR23_avg', 'logO32_min', 'logO32_max', 'logO32_avg']
 
 
