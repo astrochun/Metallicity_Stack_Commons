ba79f4182103e4a8b6bd2dd36b742fb9cc7ba752
Chun Ly <astro.chun@gmail.com>
2020-02-22 09:22:05 -0700

    column_names: Shorten key name in filename_dict: bin_fit_revised -> bin_fit_rev (iss #25)

--- Metallicity_Stack_Commons/column_names.py
+++ Metallicity_Stack_Commons/column_names.py
@@ -27,7 +27,7 @@ temp_metal_names0 = ['T_e', '12+log(O/H)', 'log(O+/H)', 'log(O++/H)', 'O+/H', 'O
 filename_dict = dict()
 filename_dict['bin_info'] = 'bin_info.tbl'
 filename_dict['bin_fit'] = 'bin_emission_line_fit.tbl'
-filename_dict['bin_fit_revised'] = filename_dict['bin_fit'].replace('.tbl', '.revised.tbl')
+filename_dict['bin_fit_rev'] = filename_dict['bin_fit'].replace('.tbl', '.revised.tbl')
 filename_dict['bin_derived_prop'] = 'bin_derived_properties.tbl'
 filename_dict['bin_derived_prop_rev'] = filename_dict['bin_derived_prop'].replace('.tbl', '.revised.tbl')
 
