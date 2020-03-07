f949e74b9bce886bc04079fa3cee1362c7a0f1de
Chun Ly <astro.chun@gmail.com>
2020-03-07 12:09:23 -0600

    ratioserror_prop_flux_ratios: Define flux ratios and return flux_ratios_dict (iss #5)

--- Metallicity_Stack_Commons/ratios.py
+++ Metallicity_Stack_Commons/ratios.py
@@ -1,4 +1,8 @@
-from . import line_name
+import numpy as np
+
+from . import line_name_short
+from .temp_metallicity_calc import R_calculation
+
 
 def error_prop_flux_ratios(flux_dict):
     """
@@ -7,7 +11,25 @@ def error_prop_flux_ratios(flux_dict):
       containing emission-line fluxes
 
     :param flux_dict: dictionary containing line ratios
-    :return:
+    :return flux_ratios_dict: dictionary containing flux ratios
     """
 
-    two_beta = flux_dict['OII_3727']
+    OII  = flux_dict[line_name_short['OII']]
+    OIII = flux_dict[line_name_short['OIII']]
+    Hb   = flux_dict[line_name_short['HB']]
+    OIII4363 = flux_dict[line_name_short['4363']]
+
+    two_beta = OII/Hb
+    three_beta = OIII/Hb
+    logR23 = np.log10(two_beta + three_beta)
+    logO32 = np.log10(OIII/OII)
+
+    flux_ratios_dict = dict()
+    flux_ratios_dict['two_beta'] = two_beta
+    flux_ratios_dict['three_beta'] = three_beta
+    flux_ratios_dict['logR23'] = logR23
+    flux_ratios_dict['logO32'] = logO32
+
+    R_calculation(OIII4363, OIII)
+
+    return flux_ratios_dict
