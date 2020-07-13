3b854c6b36d72753c48fa8896907dcc7052170fb
Chun Ly <astro.chun@gmail.com>
2020-07-13 16:31:41 -0700

    Remove pylabe requirement; bump version

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setup(
     name='Metallicity_Stack_Commons',
-    version='1.0.1',
+    version='1.1.4',
     packages=['Metallicity_Stack_Commons'],
     url='https://github.com/astrochun/Metallicity_Stack_Commons',
     license='MIT License',
     author='Chun Ly',
     author_email='astro.chun@gmail.com',
     description='Set of common codes used in metallicity studies that use stacking techniques',
-    long_description = long_description,
+    long_description=long_description,
     long_description_content_type = "text/markdown",
-    install_requires = ['numpy', 'astropy', 'matplotlib', 'pylab', 'scipy']
+    install_requires=['numpy', 'astropy', 'matplotlib', 'scipy']
 )
