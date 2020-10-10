503728c1f358eb3e57f94f12b20b6fa247e934b9
Chun Ly <astro.chun@gmail.com>
2020-10-10 15:03:42 -0700

    error_prop.fluxes_derived_prop: Add raw keyword option #67
    Simple calculation without randomization, dust attenuation

--- Metallicity_Stack_Commons/analysis/error_prop.py
+++ Metallicity_Stack_Commons/analysis/error_prop.py
@@ -5,7 +5,8 @@ from .. import line_name
 from astropy.io import ascii as asc
 import numpy as np
 
-from ..column_names import filename_dict, temp_metal_names0, npz_filename_dict
+from ..column_names import filename_dict, temp_metal_names0, npz_filename_dict, \
+    bin_names0
 from .ratios import flux_ratios
 from .temp_metallicity_calc import temp_calculation, metallicity_calculation
 from .attenuation import compute_EBV
@@ -31,7 +32,7 @@ def write_npz(path, npz_files, dict_list):
         np.savez(npz_outfile, **dict_input)
 
 
-def fluxes_derived_prop(path, binned_data=True, apply_dust=False, revised=True):
+def fluxes_derived_prop(path, raw=False, binned_data=True, apply_dust=False, revised=True):
     """
     Purpose:
       Use measurements and their uncertainties to perform a randomization
@@ -40,6 +41,7 @@ def fluxes_derived_prop(path, binned_data=True, apply_dust=False, revised=True):
       determines electron temperature and metallicity
 
     :param path: str of full path
+    :param raw: bool to do a simple calculation, no randomization. Default: False
     :param binned_data: bool for whether to analysis binned data. Default: True
     :param apply_dust: bool for whether to apply dust attenuation. Default: False
     :param revised: bool to indicate if revised validation table is used. Default: True
@@ -48,31 +50,34 @@ def fluxes_derived_prop(path, binned_data=True, apply_dust=False, revised=True):
     # Define files to read in for binned data
     if binned_data:
         flux_file = join(path, filename_dict['bin_fit'])
-        prop_file = join(path, filename_dict['bin_derived_prop'])
-        if revised:
-            print('Using REVISED validation table')
-            verify_file = join(path, filename_dict['bin_valid_rev'])
-        else:
-            print('Using validation table')
-            verify_file = join(path, filename_dict['bin_valid'])
+        if not raw:
+            prop_file = join(path, filename_dict['bin_derived_prop'])
+            if revised:
+                print('Using REVISED validation table')
+                verify_file = join(path, filename_dict['bin_valid_rev'])
+            else:
+                print('Using validation table')
+                verify_file = join(path, filename_dict['bin_valid'])
 
     flux_tab0 = asc.read(flux_file)
-    prop_tab0 = asc.read(prop_file)
+    if not raw:
+        prop_tab0 = asc.read(prop_file)
 
     if binned_data:
-        verify_tab = asc.read(verify_file)
-        detection = verify_tab['Detection'].data
+        if not raw:
+            verify_tab = asc.read(verify_file)
+            detection = verify_tab['Detection'].data
 
-        # For now we are only considering those with reliable detection and
-        # excluding those with reliable non-detections (detect = 0.5)
-        detect_idx = np.where((detection == 1))[0]
+            # For now we are only considering those with reliable detection and
+            # excluding those with reliable non-detections (detect = 0.5)
+            detect_idx = np.where((detection == 1))[0]
 
-        ID = verify_tab['bin_ID'].data
-        ID_detect = ID[detect_idx]
-        print(ID_detect)
+            ID = verify_tab['bin_ID'].data
+            ID_detect = ID[detect_idx]
+            print(ID_detect)
 
-        flux_tab = flux_tab0[detect_idx]
-        prop_tab = prop_tab0[detect_idx]
+            flux_tab = flux_tab0[detect_idx]
+            prop_tab = prop_tab0[detect_idx]
 
     flux_cols     = [str0+'_Flux_Gaussian' for str0 in line_name]
     flux_rms_cols = [str0+'_RMS' for str0 in line_name]
@@ -81,104 +86,133 @@ def fluxes_derived_prop(path, binned_data=True, apply_dust=False, revised=True):
     # EMISSION-LINE SECTION
     #
 
-    # Initialize dictionaries
-    flux_pdf_dict = dict()
-    flux_peak_dict = dict()
-    flux_error_dict = dict()
+    if raw:
+        flux_dict = dict()
+        for aa, flux, rms in zip(range(len(flux_cols)), flux_cols, flux_rms_cols):
 
-    # Randomization for emission-line fluxes
-    for aa, flux, rms in zip(range(len(flux_cols)), flux_cols, flux_rms_cols):
-        flux_pdf = random_pdf(flux_tab[flux], flux_tab[rms], seed_i=aa,
-                              n_iter=1000)
-        err, peak = compute_onesig_pdf(flux_pdf, flux_tab[flux], usepeak=True)
+            # Fill in dictionary
+            flux_dict[line_name[aa]] = flux_tab0[flux].data
 
-        # Fill in dictionary
-        flux_pdf_dict[line_name[aa]] = flux_pdf
-        flux_peak_dict[line_name[aa] + '_peak'] = peak
-        flux_error_dict[line_name[aa] + '_error'] = err
+        flux_ratios_dict = flux_ratios(flux_dict)
 
-        # Update values
-        flux_tab0[line_name[aa] + '_Flux_Gaussian'][detect_idx] = peak
+        derived_prop_dict = dict()
 
-    # Write revised emission-line fit ASCII table
-    new_flux_file = join(path, filename_dict['bin_fit_rev'])
-    if exists(new_flux_file):
-        print("Overwriting: "+new_flux_file)
+        Te = temp_calculation(flux_ratios_dict['R'])
+        derived_prop_dict[temp_metal_names0[0]] = Te
+        metal_dict = metallicity_calculation(Te, flux_ratios_dict['two_beta'],
+                                             flux_ratios_dict['three_beta'])
+        derived_prop_dict.update(metal_dict)
+
+        # Write Astropy table
+        # Get bin IDs and composite measurements in one dicitonary and write to table
+        tbl_dict = {bin_names0[0]: np.int_(flux_tab0[bin_names0[0]].data)}
+        tbl_dict.update(flux_ratios_dict)
+        tbl_dict.update(derived_prop_dict)
+
+        outfile = join(path, filename_dict['bin_derived_prop'])
+        print("Writing : ", outfile)
+        asc.write(tbl_dict, output=outfile, overwrite=True,
+                  format='fixed_width_two_line')
     else:
-        print("Writing: "+new_flux_file)
-    asc.write(flux_tab0, new_flux_file, overwrite=True,
-              format='fixed_width_two_line')
+        # Initialize dictionaries
 
-    # Save flux npz files
-    npz_files = [npz_filename_dict['flux_pdf'],
-                 npz_filename_dict['flux_errors'],
-                 npz_filename_dict['flux_peak']]
-    dict_list = [flux_pdf_dict, flux_error_dict, flux_peak_dict]
-    write_npz(path, npz_files, dict_list)
+        flux_pdf_dict = dict()
+        flux_peak_dict = dict()
+        flux_error_dict = dict()
 
-    # Obtain line ratio distributions: logR23, logO32, two_beta, three_beta, R
-    # Also include Balmer decrements
-    flux_ratios_dict = flux_ratios(flux_pdf_dict)
+        # Randomization for emission-line fluxes
+        for aa, flux, rms in zip(range(len(flux_cols)), flux_cols, flux_rms_cols):
+            flux_pdf = random_pdf(flux_tab[flux], flux_tab[rms], seed_i=aa,
+                                  n_iter=1000)
+            err, peak = compute_onesig_pdf(flux_pdf, flux_tab[flux], usepeak=True)
 
-    #
-    # Get EBV from Balmer decrement if apply_dust set
-    #
+            # Fill in dictionary
+            flux_pdf_dict[line_name[aa]] = flux_pdf
+            flux_peak_dict[line_name[aa] + '_peak'] = peak
+            flux_error_dict[line_name[aa] + '_error'] = err
 
-    if apply_dust:
-        EBV = compute_EBV(flux_ratios_dict['HgHb'], source='HgHb')
-    else:
-        EBV = None
+            # Update values
+            flux_tab0[line_name[aa] + '_Flux_Gaussian'][detect_idx] = peak
 
-    #
-    # DERIVED PROPERTIES SECTION
-    #
+        # Write revised emission-line fit ASCII table
+        new_flux_file = join(path, filename_dict['bin_fit_rev'])
+        if exists(new_flux_file):
+            print("Overwriting: "+new_flux_file)
+        else:
+            print("Writing: "+new_flux_file)
+        asc.write(flux_tab0, new_flux_file, overwrite=True,
+                  format='fixed_width_two_line')
 
-    # Initialize dictionaries
-    derived_prop_pdf_dict = dict()
-    derived_prop_error_dict = dict()
-    derived_prop_peak_dict = dict()
+        # Save flux npz files
+        npz_files = [npz_filename_dict['flux_pdf'],
+                     npz_filename_dict['flux_errors'],
+                     npz_filename_dict['flux_peak']]
+        dict_list = [flux_pdf_dict, flux_error_dict, flux_peak_dict]
+        write_npz(path, npz_files, dict_list)
 
-    # Calculate temperature distribution
-    Te_pdf = temp_calculation(flux_ratios_dict['R'], EBV=EBV)
-    derived_prop_pdf_dict[temp_metal_names0[0]] = Te_pdf
+        # Obtain line ratio distributions: logR23, logO32, two_beta, three_beta, R
+        # Also include Balmer decrements
+        flux_ratios_dict = flux_ratios(flux_pdf_dict)
 
-    # Calculate metallicity distribution
-    metal_dict = metallicity_calculation(Te_pdf, flux_ratios_dict['two_beta'],
-                                         flux_ratios_dict['three_beta'],
-                                         EBV=EBV)
-    derived_prop_pdf_dict.update(metal_dict)
+        #
+        # Get EBV from Balmer decrement if apply_dust set
+        #
 
-    # Loop for each derived properties (T_e, metallicity, etc.)
-    for names0 in temp_metal_names0:
-        arr0 = prop_tab[names0].data
+        if apply_dust:
+            EBV = compute_EBV(flux_ratios_dict['HgHb'], source='HgHb')
+        else:
+            EBV = None
 
-        err_prop, peak_prop = \
-            compute_onesig_pdf(derived_prop_pdf_dict[names0], arr0,
-                               usepeak=True)
+        #
+        # DERIVED PROPERTIES SECTION
+        #
 
-        # Fill in dictionary
-        derived_prop_error_dict[names0+'_error'] = err_prop
-        derived_prop_peak_dict[names0+'_peak'] = peak_prop
+        # Initialize dictionaries
+        derived_prop_pdf_dict = dict()
+        derived_prop_error_dict = dict()
+        derived_prop_peak_dict = dict()
 
-        # Update values
-        prop_tab0[names0][detect_idx] = peak_prop
+        # Calculate temperature distribution
+        Te_pdf = temp_calculation(flux_ratios_dict['R'], EBV=EBV)
+        derived_prop_pdf_dict[temp_metal_names0[0]] = Te_pdf
 
-    # Write revised properties ASCII table
-    if not apply_dust:
-        new_prop_file = join(path, filename_dict['bin_derived_prop_rev'])
-    else:
-        new_prop_file = join(path, filename_dict['bin_derived_prop_rev_dust'])
-    if exists(new_prop_file):
-        print("Overwriting: "+new_prop_file)
-    else:
-        print("Writing: "+new_prop_file)
-    asc.write(prop_tab0, new_prop_file, overwrite=True,
-              format='fixed_width_two_line')
+        # Calculate metallicity distribution
+        metal_dict = metallicity_calculation(Te_pdf, flux_ratios_dict['two_beta'],
+                                             flux_ratios_dict['three_beta'],
+                                             EBV=EBV)
+        derived_prop_pdf_dict.update(metal_dict)
 
-    # Save derived properties npz files
-    npz_files = [npz_filename_dict['der_prop_pdf'],
-                 npz_filename_dict['der_prop_errors'],
-                 npz_filename_dict['der_prop_peak']]
-    dict_list = [derived_prop_pdf_dict, derived_prop_error_dict,
-                 derived_prop_peak_dict]
-    write_npz(path, npz_files, dict_list)
+        # Loop for each derived properties (T_e, metallicity, etc.)
+        for names0 in temp_metal_names0:
+            arr0 = prop_tab[names0].data
+
+            err_prop, peak_prop = \
+                compute_onesig_pdf(derived_prop_pdf_dict[names0], arr0,
+                                   usepeak=True)
+
+            # Fill in dictionary
+            derived_prop_error_dict[names0+'_error'] = err_prop
+            derived_prop_peak_dict[names0+'_peak'] = peak_prop
+
+            # Update values
+            prop_tab0[names0][detect_idx] = peak_prop
+
+        # Write revised properties ASCII table
+        if not apply_dust:
+            new_prop_file = join(path, filename_dict['bin_derived_prop_rev'])
+        else:
+            new_prop_file = join(path, filename_dict['bin_derived_prop_rev_dust'])
+        if exists(new_prop_file):
+            print("Overwriting: "+new_prop_file)
+        else:
+            print("Writing: "+new_prop_file)
+        asc.write(prop_tab0, new_prop_file, overwrite=True,
+                  format='fixed_width_two_line')
+
+        # Save derived properties npz files
+        npz_files = [npz_filename_dict['der_prop_pdf'],
+                     npz_filename_dict['der_prop_errors'],
+                     npz_filename_dict['der_prop_peak']]
+        dict_list = [derived_prop_pdf_dict, derived_prop_error_dict,
+                     derived_prop_peak_dict]
+        write_npz(path, npz_files, dict_list)
