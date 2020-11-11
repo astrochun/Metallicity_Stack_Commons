fluxes_derived_prop: Bug in EBV_HdHb computation with raw=True #67

Metallicity_Stack_Commons/analysis/error_prop.py
@@ -124,7 +124,7 @@ def fluxes_derived_prop(path, raw=False, binned_data=True, apply_dust=False,
 
         if apply_dust:
             EBV = compute_EBV(flux_ratios_dict[dust[0]], source=dust[0])
-            EBV_HdHb = compute_EBV(flux_ratios_dict[dust[1]], source=dust[0])
+            EBV_HdHb = compute_EBV(flux_ratios_dict[dust[1]], source=dust[1])
         else:
             EBV = None
             EBV_HdHb = None
@@ -230,6 +230,7 @@ def fluxes_derived_prop(path, raw=False, binned_data=True, apply_dust=False,
                         dust[2] + '_high_err': np.repeat(np.nan, len(prop_tab0))}
 
             EBV, EBV_peak = compute_EBV(flux_ratios_dict[dust[0]], source=dust[0])
+            print(EBV_peak)
 
             err_prop, peak_prop = compute_onesig_pdf(EBV, EBV_peak, usepeak=True)
 
@@ -245,6 +246,7 @@ def fluxes_derived_prop(path, raw=False, binned_data=True, apply_dust=False,
                              dust[3] + '_high_err': np.repeat(np.nan, len(prop_tab0))}
 
             EBV_HdHb, EBV_HdHb_peak = compute_EBV(flux_ratios_dict[dust[1]], source=dust[1])
+            print(EBV_HdHb_peak)
 
             err_prop, peak_prop = compute_onesig_pdf(EBV_HdHb, EBV_HdHb_peak, usepeak=True)
 