9698e526d18c1d0e67540d8037574be916f12fd4
Chun Ly <astro.chun@gmail.com>
2020-11-22 16:26:29 -0700

    Bump version: 1.1.6 -> 1.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='Metallicity_Stack_Commons',
-    version='1.1.6',
+    version='1.2.0',
     packages=['Metallicity_Stack_Commons'],
     url='https://github.com/astrochun/Metallicity_Stack_Commons',
     license='MIT License',
