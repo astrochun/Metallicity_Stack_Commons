7919fa6177c0695d546922048f3af29dfe8f68d4
Chun Ly <astro.chun@gmail.com>
2021-01-04 09:55:54 -0700

    Update output table for compatibility with valid_table module

--- Metallicity_Stack_Commons/others/extract_deep2_data.py
+++ Metallicity_Stack_Commons/others/extract_deep2_data.py
@@ -24,30 +24,41 @@ def main(infile: str, log=None):
     log.info(f"Reading : {infile}")
     orig_tab = fits.getdata(infile)
 
-    flux_cols     = [str0 + '_Flux_Gaussian' for str0 in line_name]
+    gauss_cols = [str0 + '_Flux_Gaussian' for str0 in line_name]
+    obs_cols = [str0 + '_Flux_Observed' for str0 in line_name]
     flux_rms_cols = [str0 + '_RMS' for str0 in line_name]
+    sigma_cols = [str0 + '_Sigma' for str0 in line_name]
+    snr_cols = [str0 + '_S/N' for str0 in line_name]
 
-    # Column name in orig_table corresponding to flux_cols
+    # Column name in orig_table corresponding to gauss_cols
     prefixes = ['OII', 'HD', 'HG', 'OIIIA', 'HB', 'OIIIB', 'OIIIR']
-    orig_flux_cols = [str0 + '_FLUX_MOD' for str0 in prefixes]
+    orig_gauss_cols = [str0 + '_FLUX_MOD' for str0 in prefixes]
+    orig_obs_cols = [str0 + '_FLUX_DATA' for str0 in prefixes]
+    orig_sigma_cols = [str0 + '_SIGMA' for str0 in prefixes]
     orig_snr_cols = [str0 + '_SNR' for str0 in prefixes]
 
     reformatted_tab = Table()
     reformatted_tab['bin_ID'] = 1 + np.arange(len(orig_tab))
 
-    for ii in range(len(flux_cols)):
-        flux = orig_tab[orig_flux_cols[ii]]
+    for ii in range(len(gauss_cols)):
+        flux = orig_tab[orig_gauss_cols[ii]]
         rms  = flux / orig_tab[orig_snr_cols[ii]]
-        reformatted_tab[flux_cols[ii]] = flux
+        # Widths are in observed frame, shift to rest-frame
+        gauss_sig = orig_tab[orig_sigma_cols[ii]] / (1+orig_tab['ZSPEC'])
+        reformatted_tab[gauss_cols[ii]] = flux
+        reformatted_tab[obs_cols[ii]] = orig_tab[orig_obs_cols[ii]]
         reformatted_tab[flux_rms_cols[ii]] = rms
+        reformatted_tab[sigma_cols[ii]] = gauss_sig
+        reformatted_tab[snr_cols[ii]] = orig_tab[orig_snr_cols[ii]]
 
     out_dir = "tests_data/DEEP2_Ly2015"
     output_table = "DEEP2_Ly2015_fluxes.tbl"
     out_file = join(out_dir, output_table)
     log.info(f"Writing: {out_file}")
-    reformatted_tab.write(out_file, format='ascii.fixed_width_two_line')
+    reformatted_tab.write(out_file, format='ascii.fixed_width_two_line',
+                          overwrite=True)
 
-    symlink_file = f"tests_data/DEEP2_Ly2015/{filename_dict['bin_fit']}"
+    symlink_file = join(out_dir, filename_dict['bin_fit'])
     if not exists(symlink_file):
         symlink(output_table, symlink_file)
         log.info(f"Symlink created to : {output_table}")
