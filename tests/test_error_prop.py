a784df67b3f6d5d44e3b3f18546b93ec6d98c90e
Chun Ly <astro.chun@gmail.com>
2021-01-04 16:01:43 -0700

    test_error_prop: Remove binned_data from dict

--- tests/test_error_prop.py
+++ tests/test_error_prop.py
@@ -9,37 +9,30 @@ def test_fluxes_derived_prop():
     # it's binned data.
     params_list = [
         {'raw': True,  # No MC, no dust, use default valid_table
-         'binned_data': True,
          'apply_dust': False,
          'revised': False},
         {'raw': True,  # No MC, no dust, use revised valid_table
-         'binned_data': True,
          'apply_dust': False,
          'revised': True},
         {'raw': True,  # No MC, apply dust, use default valid_table
-         'binned_data': True,
          'apply_dust': True,
          'revised': False},
         {'raw': True,  # No MC, apply dust, use revised valid_table
-         'binned_data': True,
          'apply_dust': True,
          'revised': True},
         {'raw': False,  # MC, no dust, use default valid_table
-         'binned_data': True,
          'apply_dust': False,
          'revised': False},
         {'raw': False,  # MC, no dust, use revised valid_table
-         'binned_data': True,
          'apply_dust': False,
          'revised': True},
         {'raw': False,  # MC, apply dust, use default valid_table
-         'binned_data': True,
          'apply_dust': True,
          'revised': False},
         {'raw': False,  # MC, apply dust, use revised valid_table
-         'binned_data': True,
          'apply_dust': True,
          'revised': True},
     ]
     for params in params_list:
-        error_prop.fluxes_derived_prop(deep2_dir, **params, verbose=True)
+        error_prop.fluxes_derived_prop(deep2_dir, **params, binned_data=True,
+                                       verbose=True)
